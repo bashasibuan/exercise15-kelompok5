0a120582eb31dcc2f2e6f604c3b02295c61ae847
bashasibuan <46988897+bashasibuan@users.noreply.github.com>
2023-09-19 22:28:15 +0700

    soal4

--- ibas.js
+++ ibas.js
@@ -43,3 +43,33 @@ function gcd_two_numbers(x, y) {
   }
 
   console.log(lcm(20, 15));
+
+  //soal4
+  const test = (str) => { 
+    if (str.length === 0) 
+     {
+     return 'String should not be empty!'
+    }
+     if (typeof str !== 'string')
+      {
+        return 'It must be a string.'
+      }
+   const occurrence_Map = new Map()
+ 
+   for (const char of str) 
+     {
+       occurrence_Map.set(char, occurrence_Map.get(char) + 1 || 1)
+     }
+ 
+ 
+   let max_char = { char: '', occur: -Infinity }
+ 
+   for (const [char, occur] of occurrence_Map) {
+     if (occur > max_char.occur) {
+       max_char = { char,occur }
+     }
+   }
+ 
+   return max_char.char
+ }
+ console.log(test("Madam"))
