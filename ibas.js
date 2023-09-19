5011d6ee57aca66a572df34694539db69735efb7
bashasibuan <46988897+bashasibuan@users.noreply.github.com>
2023-09-19 15:47:02 +0700

    soal2

--- ibas.js
+++ ibas.js
@@ -12,4 +12,19 @@ else {
 }
 return result;
 }
-console.log(checkwk("21-01-2021"))
+console.log(checkwk("21-01-2021"));
+
+//Soal 2
+function gcd_two_numbers(x, y) {
+    if ((typeof x !== 'number') || (typeof y !== 'number')) 
+      return false;
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while(y) {
+      var t = y;
+      y = x % y;
+      x = t;
+    }
+    return x;
+  }
+  console.log(gcd_two_numbers(12, 13));
