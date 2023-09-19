ec6f1daca415ea7e7aa8c541420a868b6530c52a
Murp13 <suryarifin55@gmail.com>
2023-09-19 15:16:20 +0700

    sorting string

---
+++ surya.js
@@ -0,0 +1,26 @@
+export function tanpaSort(inputString) {
+
+    const char = inputString.split('');
+    
+
+    for (let i = 0; i < char.length - 1; i++) {
+      for (let j = 0; j < char.length - i - 1; j++) {
+        if (char[j] > char[j + 1]) {
+    
+          const temp = char[j];
+          char[j] = char[j + 1];
+          char[j + 1] = temp;
+        }
+      }
+    }
+
+    return char.join('');
+  }
+
+export function pakaiSort(inputString) {
+
+    const char = inputString.split('');
+
+    return char.sort().join('');
+  }
+
