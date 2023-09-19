f91806f985d51583617145c5629b4d9a8f7fbf71
Murp13 <suryarifin55@gmail.com>
2023-09-19 15:23:00 +0700

    hitung array

---
+++ surya.js
@@ -0,0 +1,13 @@
+function calculateArray(arr) {
+    let sum = 0;
+  
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+        sum += arr[i][j];
+      }
+    }
+  
+    return sum;
+  }
+
+  export default calculateArray();
