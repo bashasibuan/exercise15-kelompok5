408435cc9d2458ad8c11d41727d5164d1c42a1da
Murp13 <suryarifin55@gmail.com>
2023-09-19 15:19:20 +0700

    minimal tanggal

---
+++ surya.js
@@ -0,0 +1,9 @@
+function tanggalMin() {
+	// let maximumDate = new Date(Math.max.apply(null, dates));
+	let minimDate = new Date(Math.min.apply(null, dates));
+
+	// console.log("Max date is - " + maximumDate);
+	console.log("Min date is - " + minimDate);
+}
+
+export default tanggalMin();
